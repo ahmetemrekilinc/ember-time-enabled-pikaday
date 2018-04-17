1bdefe50f4f0c0e9e700335339730759a1992c5c
Ahmet Emre Kılınç <ahmet.kilinc@intra.local>
2018-04-17 16:37:05 +0300

    3.0.1

--- addon/time-enabled-pikaday-inputless/component.js
+++ addon/time-enabled-pikaday-inputless/component.js
@@ -6,21 +6,21 @@ export default Pikaday.extend({
 
     didInsertElement() {
       this._super(...arguments);
-            let timeEnabled = (this.get('timeEnabled') === true);
-            let use24hour = (this.get('use24hour') === true);
-            let showMinutes = (this.get('showMinutes') !== false);
-            let showSeconds = (this.get('showSeconds') === true);
+      let timeEnabled = (this.get('timeEnabled') === true);
+      let use24hour = (this.get('use24hour') === true);
+      let showMinutes = (this.get('showMinutes') !== false);
+      let showSeconds = (this.get('showSeconds') === true);
 
-            let timeLabel = '';
-            if(!isBlank(this.get('timeLabel'))){
-              timeLabel = this.get('timeLabel');
-            }
+      let timeLabel = '';
+      if(!isBlank(this.get('timeLabel'))){
+        timeLabel = this.get('timeLabel');
+      }
 
-            set(this.get('pikaday._o'), 'showTime', timeEnabled);
-            set(this.get('pikaday._o'), 'showMinutes', showMinutes);
-            set(this.get('pikaday._o'), 'showSeconds', showSeconds);
-            set(this.get('pikaday._o'), 'use24hour', use24hour);
-            set(this.get('pikaday._o'), 'timeLabel', timeLabel);
+      set(this.get('pikaday._o'), 'showTime', timeEnabled);
+      set(this.get('pikaday._o'), 'showMinutes', showMinutes);
+      set(this.get('pikaday._o'), 'showSeconds', showSeconds);
+      set(this.get('pikaday._o'), 'use24hour', use24hour);
+      set(this.get('pikaday._o'), 'timeLabel', timeLabel);
     }
 
 });
